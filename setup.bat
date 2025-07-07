470d84ceb798892a3cf6c65b7575e74f35480ec3
tonyhawq <larrylarsonbest@gmail.com>
2025-07-07 00:01:13 -0700

    adds python installation checker

--- setup.bat
+++ setup.bat
@@ -7,6 +7,15 @@ echo           2.5GB of python libraries!
 echo ------------------------------------------------------
 echo The script will let you know when installing is done!
 echo ------------------------------------------------------
+
+python --version >nul 2>&1 && (
+    python --version
+) || (
+    echo Python not found. Install python from https://www.python.org/downloads/ or run the included python install script.
+    pause
+    exit /b 1
+)
+
 echo[
 if exist venv (
     echo Skipping virtual environment creation...
