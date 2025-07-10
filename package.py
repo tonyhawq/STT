eecef0fcb97c372a60173d9f7f32b5b31d6ed21c
tonyhawq <larrylarsonbest@gmail.com>
2025-07-10 13:26:04 -0700

    adds version file to packaged result

--- package.py
+++ package.py
@@ -21,7 +21,11 @@ if __name__ == "__main__":
         print("Does not match pattern \"x.x.x\".")
     print(f"Packaging version \"{version}\"")
     zip_filename = "releases/STT_"+version+".zip"
+    if os.path.exists(zip_filename):
+        print("Release already exists.")
+        exit(1)
     with zipfile.ZipFile(zip_filename, "w", zipfile.ZIP_DEFLATED) as zipf:
+        zipf.writestr(zinfo_or_arcname="version.number", data=version)
         for file in files_to_package:
             print("Packaging", file)
             zipf.write(file, file)
