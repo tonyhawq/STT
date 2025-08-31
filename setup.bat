c78f28fe8006a4324b7c7c997929f7de3d88e279
tonyhawq <larrylarsonbest@gmail.com>
2025-08-31 00:44:17 -0700

    python installer in setup

--- setup.bat
+++ setup.bat
@@ -8,17 +8,26 @@ echo ------------------------------------------------------
 echo The script will let you know when installing is done!
 echo ------------------------------------------------------
 
-python --version >nul 2>&1 && (
-    python --version
-) || (
-    echo Python not found. Install python 3.11 from https://www.python.org/downloads/release/python-3119/.
-    pause
-    exit /b 1
+python --version >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Python not found.
+    SETLOCAL ENABLEDELAYEDEXPANSION
+    set /p install_choice="Do you want to run the built-in python installer? (Y/N)"
+    if /i !install_choice! == "Y" (
+        echo Installing python...
+        start /wait "" "%~dp0\embedded\python-3.11.9-amd64.exe" /quiet InstallAllUsers=1 PrependPath=1
+    ) ELSE (
+        echo Install python 3.11 from https://www.python.org/downloads/release/python-3119/.
+        echo Make sure to add python to PATH and disable path length limits.
+        pause
+        exit /b 1
+    )
 )
 
 python -c "import sys; exit(0) if sys.version_info.major == 3 and sys.version_info.minor == 11 else exit(1)"
 if %errorlevel% neq 0 (
-    echo Python 3.11 is required.
+    echo Python 3.11 is required. Current python version is
+    python --version
     pause
     exit /b 1
 )
